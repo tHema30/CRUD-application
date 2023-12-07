fac7f0a78e50bff30266425202de703591584689
tHema30 <tkematharsini03@gmail.com>
2023-12-07 15:11:41 +0530

    CRUD application practice file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-require('dotenv').config();
+require('dotenv').config();/*Dotenv is a npm package which help us to import
+ environment variables file .It will be available at Every place 
+ if we require it in app file.This line simply means importing environment variables*/
 
-const express = require('express');
+const express = require('express');/*This declares a constant variable named 'express. */
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
 
